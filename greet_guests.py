0b9e13df750b43b5d3dfc7b9af55be495373802a
Laurence Roberts-Elliott <laurencejbelliott@gmail.com>
2020-01-26 16:01:41 +0000

    Guest greeting as people are detected

---
+++ greet_guests.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import PoseStamped
+from mary_tts.msg import maryttsActionGoal
+
+
+speakPub = rospy.Publisher("speak/goal", maryttsActionGoal, queue_size=1)
+
+
+def callback(data):
+    greetStr = "Hello, and welcome to the Santander signing event!"
+    greetMsg = maryttsActionGoal()
+    greetMsg.goal.text = greetStr
+    speakPub.publish(greetMsg)
+    print("Nearby person greeted with: \"" + greetStr + "\"")
+    # time until another greeting allowed, in seconds
+    wait_time = 5
+    print("Waiting for " + str(wait_time) + " seconds before next greeting allowed...\n")
+    rospy.sleep(wait_time)
+
+
+def listener():
+    rospy.init_node('greet_guests', anonymous=False)
+
+    rospy.Subscriber("people_tracker/pose", PoseStamped, callback, queue_size=1)
+
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    listener()
